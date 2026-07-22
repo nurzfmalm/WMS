98ea3b0c77a26d78feb3af0aa069b9f228b4f144
nurzfmalm <nurzatkassymov@gmail.com>
2026-07-23 00:27:44 +0500

    Fixed

--- frontend/App.js
+++ frontend/App.js
@@ -1,4 +1,4 @@
-const API = "http://127.0.0.1:8000/api";
+const API = "http://wms-xxgh.vercel.app/api";
 const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => [...document.querySelectorAll(selector)];
 
